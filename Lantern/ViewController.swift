f7010cdf04e8883818d1614aa23dfff3fc5a130d
MacBookAir <macbookair@mila.dlink>
2022-03-28 21:48:22 +0300

    Initial Commit

---
+++ Lantern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lantern
+//
+//  Created by MacBookAir on 28.03.2022.
+//  Copyright © 2022 MacBookAir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
